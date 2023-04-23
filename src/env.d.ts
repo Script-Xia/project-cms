806167f2df4dc02cdfcfaca850729d0e3dba36b9
JackieXia-joy <1781106042@qq.com>
2023-04-23 11:28:48 +0800

    chore(package.json): 添加/更新依赖

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 // <reference types="vite/client"></reference> </reference>
 
 interface ImportMetaEnv {
-  readonly MODE: string,
-  readonly VITE_BASE_URL: string,
+  readonly MODE: string
+  readonly VITE_BASE_URL: string
   readonly VITE_TIME_OUT: number
 }
 
