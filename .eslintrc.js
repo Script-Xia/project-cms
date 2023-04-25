35df3ba187c270feb4801943d35bf19c1846d95a
JackieXia-joy <1781106042@qq.com>
2023-04-25 15:58:58 +0800

    chore(eslint/tsconfig): 修改eslint的配置以跳过某些类型检测

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    // 在setup中使用宏（defineExpose、defineEmit...）时跳过类型检测
+    "vue/setup-compiler-macros": true
   },
   extends: [
     "plugin:vue/vue3-essential",
@@ -17,6 +19,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    // 跳过 any 检测，在某些情况下需要用到 any
+    "@typescript-eslint/no-explicit-any": "off",
+    // 跳过检测没使用的变量，在引入组件后也会警告，所以关闭
+    "@typescript-eslint/no-unused-vars": "off",
+    // v3建议定义组件名时使用多词组合以避免与 HTML 标签名重复
+    "vue/multi-word-component-names": "off"
   }
 }
